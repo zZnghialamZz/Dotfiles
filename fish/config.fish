0047c78677b1ba1cee86c78022807a09ee7c7412
Nghia Lam <nghialam12795@gmail.com>
2023-11-05 11:50:56 +0700

    Fish terminal setup for macos

---
+++ fish/config.fish
@@ -0,0 +1,24 @@
+# aliases
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll -A"
+alias g git
+command -qv nvim && alias vim nvim
+
+set -gx EDITOR nvim
+
+switch (uname)
+  case Darwin
+    source (dirname (status --current-filename))/config-osx.fish
+  case Linux
+    source (dirname (status --current-filename))/config-linux.fish
+  case '*'
+    source (dirname (status --current-filename))/config-windows.fish
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
