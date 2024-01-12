19dbf6ffe44850faf486ff109a3d37fb66bf3b57
Nghia Lam <nghialam12795@gmail.com>
2024-01-12 09:55:02 +0700

    Latex config for doom emacs

--- doom/init.el
+++ doom/init.el
@@ -153,7 +153,9 @@
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       ;;latex             ; writing papers in Emacs has never been so fun
+       (latex             ; writing papers in Emacs has never been so fun
+        +cdlatex
+        +fold)
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        (lua
